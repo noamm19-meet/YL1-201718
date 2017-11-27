3ee0bba0739ebee2318234a7eadbcc9cc89fa6e3
tutim2000@gamil.com <tamerk19@meet.mit.edu>
2017-11-27 19:01:35 +0200

    lab5

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -1,15 +1,47 @@
-from turtle import Turtle,colormode
+from turtle import Turtle
+from turtle import *
+colormode(255)
 import random
- 
+import turtle
+
+def r():
+    turtle.hideturtle()
+    for i in range(6):
+        turtle.forward(100)
+        turtle.left(60)
+
+    
+
 class Square(Turtle):
     def __init__ (self, size):
-        
         Turtle.__init__(self)
         self.shapesize(size)
         self.shape("square")
-        def randome_color(self):
-            
-            self=random.randint(colormode)
+    def random_color(self):
+        r=random.randint(0,255)
+        g=random.randint(0,255)
+        b=random.randint(0,255)
+        self.color(r,g,b)
 
+
+##class NShape(Turtle):
+##    def __init__ (self):
+##        Turtle.__init__(self):
+            
 
 sq = Square(5)
+sq= sq.random_color()
+
+
+class Hexagone(Turtle):
+    def __init__(self,size):
+        Turtle.__init__(self)
+        self.shape(r())
+        r1=random.randint(0,255)
+        g1=random.randint(0,255)
+        b1=random.randint(0,255)
+        self.color(r1,g1,b1)
+        
+
+
+hx =  Hexagone(10)   
